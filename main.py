1fb2c68943b1b32fe53ce248ee999c988154e1a5
invcble <imonbera13@gmail.com>
2024-03-29 03:54:02 -0400

    Update

--- main.py
+++ main.py
@@ -36,7 +36,13 @@ def refresh_window_list():
     global window_list_dropdown, windowList
     windowList_strings = [f"{title[0:54]}" for hwnd, title in windowList] #Dirty truncate to resize drop-down UI
     window_list_dropdown.configure(values = windowList_strings)
+
+def change_appearance_mode_event(new_appearance_mode: str):
+    ctk.set_appearance_mode(new_appearance_mode)
    
+
+ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 panel = ctk.CTk()
 panel.geometry("400x400+"+ str(int(screen_width/2) - 200) + '+' + str(int(screen_height/2) - 200))
@@ -55,6 +61,12 @@ submit_button.pack(pady=20)
 
 window_list_dropdown = ctk.CTkComboBox(panel, values = ["Select Application"], width = 400)
 window_list_dropdown.pack(padx=20, pady=(20, 10))
+
+toggle_mode = ctk.CTkLabel(panel, text="Appearance Mode:", anchor="w")
+toggle_mode.pack(padx=20, pady=(10, 0))
+
+toggle_mode_options = ctk.CTkOptionMenu(panel, values=[ "System", "Light", "Dark" ], command = change_appearance_mode_event)
+toggle_mode_options.pack(padx=20, pady=(10, 0))
 
 Thread(target = update_window_list).start()
 
