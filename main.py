ef4971aa3240727b020dd0a5b05c1978649def1f
invcble <imonbera13@gmail.com>
2024-03-29 00:00:13 -0400

    Update

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import tkinter
-import sv_ttk
-from tkinter import ttk
-from tkinter import messagebox
-import xmlrpc.client
-import time
-import subprocess
-from threading import Thread
-import sys
-import os
+import customtkinter as ctk
+import darkdetect
 import ctypes
+
 
 user32 = ctypes.windll.user32
 user32.SetProcessDPIAware()
@@ -18,18 +11,19 @@ screen_height = user32.GetSystemMetrics(1)
 
 
 
-panel = tkinter.Tk()
+panel = ctk.CTk()
 panel.geometry("400x400+"+ str(int(screen_width/2) - 200) + '+' + str(int(screen_height/2) - 200))
 panel.resizable(False, False)
-sv_ttk.set_theme("dark")
 
-label = ttk.Label(panel, text="Display Resolution is " + str(screen_width) + 'x' + str(screen_height))
+
+
+label = ctk.CTkLabel(panel, text="Display Resolution is " + str(screen_width) + 'x' + str(screen_height))
 label.pack(pady=20)
 
-type_field = ttk.Entry(panel)
+type_field = ctk.CTkEntry(panel)
 type_field.pack()
 
-submit_button = ttk.Button(panel, text="Submit", command= None)
+submit_button = ctk.CTkButton(panel, text="Submit", command= None)
 submit_button.pack(pady=20)
 
 panel.attributes('-topmost', True)
