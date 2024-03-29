f1e22b52e409f6eb6adadd2bbd0306c0938c9e63
invcble <imonbera13@gmail.com>
2024-03-28 22:42:31 -0400

    Update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tkinter as tk
+import tkinter
+import sv_ttk
+from tkinter import ttk
 from tkinter import messagebox
 import xmlrpc.client
 import time
@@ -11,23 +13,23 @@ import ctypes
 user32 = ctypes.windll.user32
 user32.SetProcessDPIAware()
 
-screen_width = str(user32.GetSystemMetrics(0))
-screen_height = str(user32.GetSystemMetrics(1))
+screen_width = user32.GetSystemMetrics(0)
+screen_height = user32.GetSystemMetrics(1)
 
 
 
-panel = tk.Tk()
-panel.geometry("400x400")
+panel = tkinter.Tk()
+panel.geometry("400x400+"+ str(int(screen_width/2) - 200) + '+' + str(int(screen_height/2) - 200))
 panel.resizable(False, False)
-panel.title("Control Panel")
+sv_ttk.set_theme("dark")
 
-label = tk.Label(panel, text="Welcome to NoMoreBorder\n Display Resolution is " + screen_width + 'x' + screen_height)
+label = ttk.Label(panel, text="Display Resolution is " + str(screen_width) + 'x' + str(screen_height))
 label.pack(pady=20)
 
-type_field = tk.Entry(panel)
+type_field = ttk.Entry(panel)
 type_field.pack()
 
-submit_button = tk.Button(panel, text="Submit", command= None)
+submit_button = ttk.Button(panel, text="Submit", command= None)
 submit_button.pack(pady=20)
 
 panel.attributes('-topmost', True)
